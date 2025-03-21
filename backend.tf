91376d31cb5cb8895d4c2416fb5fd6cd736e1723
Sandy <vraveendran.sandeep@gmail.com>
2025-03-21 16:39:49 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-bucket-for-tf-state"
     key = "main"
     region = "us-east-1"
-    dynamodb_table = "my-dynamo-db-table-tf"
+    dynamodb_table = "my-san-dynamo-db-table-tf"
   }
 }
